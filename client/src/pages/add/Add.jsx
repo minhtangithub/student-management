80621f989ee78bffb2dae3f818eb3ac8c5b0317c
absya23 <thachnc2002@gmail.com>
2022-05-22 08:31:57 +0700

    ui of add page, report page, setting page

--- client/src/pages/add/Add.jsx
+++ client/src/pages/add/Add.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import "./Add.scss";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import Card1 from "../../assets/card1.png";
+import Card2 from "../../assets/card2.png";
 
-import { BiRightArrow } from "react-icons/bi";
+// import { BiRightArrow } from "react-icons/bi";
+import { Card } from "../../components/Card";
 
 export const Add = () => {
   return (
     <>
       <div className="add">
         <h3>Thêm</h3>
-        <Link to="/add/add-student">
-          <i>
-            <BiRightArrow></BiRightArrow>
-          </i>
-          Tiếp nhận học sinh
-        </Link>
-        <Link to="/add/add-class">
-          {" "}
-          <i>
-            <BiRightArrow></BiRightArrow>
-          </i>
-          Lập danh sách lớp
-        </Link>
+        <div className="card-container">
+          <Card
+            img={Card1}
+            link="add/add-student"
+            name="student"
+            content="Tiếp nhận học sinh"
+          ></Card>
+          <Card
+            img={Card2}
+            link="add/add-class"
+            name="class"
+            content="Lập danh sách lớp"
+          ></Card>
+        </div>
       </div>
     </>
   );
