371b76ccebf62edf1a03220f23aab74e02d0fc4b
longvoct <86196042+longvoct@users.noreply.github.com>
2022-06-03 17:43:38 +0700

    Update cClassController.js

--- server/api/controller/cClassController.js
+++ server/api/controller/cClassController.js
@@ -42,9 +42,10 @@ const cClassController = {
   updateClass: async (req, res) => {
     try {
       const cClass = await CClass.findById(req.params.id);
-      if (req.body.grade && !cClass.grade) {
-        const grade = Grade.findById(req.body.grade);
-        await grade.updateOne({ $push: { cClasses: cClass._id } });
+      if (req.body.students) {
+        for (let i = 0; i < req.body.students.length; i++) {
+          await Student.updateOne({ cClass: req.params.id });
+        }
       }
       await cClass.updateOne({ $set: req.body });
       res.status(200).json("Updated successfully!");
